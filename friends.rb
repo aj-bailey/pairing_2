c777e7572d304e10f41947076b166b4b248f8db0
Adam Bailey <ab67319@gmail.com>
2022-11-02 17:26:02 -0600

    Define best friend and another friend

--- friends.rb
+++ friends.rb
@@ -1,7 +1,12 @@
 best_friend = "Daniel"
+another_friend = "Luana"
 
-if best_friend == "Daniel"
+if best_friend == "Daniel" && another_friend == "Luana"
     puts "You're my best friend."
+elsif another_friend == "Luana"
+    puts "Actually, you're my best friend."
+elsif best_friend == "Daniel"
+    puts "Daniel, you're my best friend."
 else
     puts "You're playing your music too loud!"
 end
